e67b4cc9b437c18f9ec9acd63b6e201dd9aff65d
David McDonald <idavidmcdonald@me.com>
2015-09-26 21:04:07 +0100

    Added simple vote page

--- public/app.js
+++ public/app.js
@@ -27,6 +27,62 @@ angular.module('eetup')
 		})
 }]);
 
-angular.module('eetup').controller(['$scope', function($scope) {
+angular.module('eetup').controller('voteCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log('hi');
+	var apiBase = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+	var apiKey = 'AIzaSyAhrpJJsHSTRNx599sBusUnK6EqXote1dk';
+	var apiOptions = {
+		location: '-33.8670,151.1957',
+		radius: 500,
+		types: 'food'
+	};
+	var apiLink = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?callback=JSON_CALLBACK&location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=' + apiKey;
 
+	// $.getJSON( apiLink, function( data ) {
+	//   console.log(data);
+	// });
+
+	// $.ajax({
+	//     type: 'GET',
+	//     url: apiLink,
+	//     async: false,
+	//     contentType: "application/json",
+	//     dataType: 'jsonp',
+	//     success: function(json) {
+	//     	console.log('entered');
+	//     	console.log(json);
+	//     },
+	//     error: function(e) {
+	//        console.log(e.message);
+	//     }
+	// });
+
+	// $http.jsonp(apiLink)
+	// 	.success(function(response){
+	// 		console.log(response);
+	// 	})
+	// 	.error(function(e){
+	// 		console.log('error');
+	// 	});
+
+
+		// $http.jsonp(apiLink).then(
+	 //        function(s) { $scope.success = JSON.stringify(s); }, 
+	 //        function(e) { $scope.error = JSON.stringify(e); } );
+		// }
+
+		// $scope.function jsonp_callback(data) {
+		//     var el = document.getElementById('ctl');
+		//     var scope = angular.element(el).scope();
+		//     scope.$apply(function() {
+		//         scope.data = JSON.stringify(data);
+		//     });
+		// }
+
+
+
+
+
+
+	console.log('hi');
 }]);
