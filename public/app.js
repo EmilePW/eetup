20e0724bca47952c57fbd810e8cd0cbdc72e4ef6
EmilePW <zcqsh61@ucl.ac.uk>
2015-09-26 17:31:40 +0100

    Added views

---
+++ public/app.js
@@ -0,0 +1,16 @@
+angular.module('eetup', ['ngRoute']);
+
+angular.module('eetup')
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+
+		// Landing page
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'homeCtrl'
+		})
+
+		.otherwise({
+			redirectTo: '/'
+		})
+}]);
